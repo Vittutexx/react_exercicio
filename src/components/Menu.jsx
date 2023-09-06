7277daea9ea1806811b2ea3730be4c50653d3610
Vittutexx <vitortex06@gmail.com>
2023-09-06 18:05:21 -0300

    Criando o arquivo Menu.jsx

---
+++ src/components/Menu.jsx
@@ -0,0 +1,15 @@
+//CRIANDO LINKS PARA NAVEGAR
+import { Link } from "react-router-dom";
+//IMPORTANDO O COMPONENTE Link DO react-router-dom.
+export default function Menu(){
+
+    return(
+        <nav className="menu">
+            <Link to="/">Home</Link>
+            <span> | </span>
+            <Link to="/produtos">Produtos</Link>    
+        </nav>
+//PASSANDO A ROTA PELO COMPONENTE Link
+
+
+    )
