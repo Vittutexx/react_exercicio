db5cc19a9c230e0cde1329022a00d9e1cf1b9746
Vittutexx <vitortex06@gmail.com>
2023-09-06 17:55:59 -0300

    Inicializando o projeto e iniciando os imports

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+//CONFIGURANDO AS ROTAS 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children:[
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/produtos",
+        element: <Produtos />
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProduto />
+      }
+    ]
+  }
+])
+  
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
